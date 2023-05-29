45e60eba8dba07a74624bba9f453c234416c04da
Ahmed Bayoumy <Ahmed.Bayoumy@linkdev.com>
2023-05-29 18:43:28 +0300

    Add Data Layer

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,25 @@
+﻿using Catalog.API.DTOs;
+using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var databaseSettings = configuration.GetValue<DatabaseSettings>("DatabaseSettings");
+            var client = new MongoClient(databaseSettings.ConnectionString);
+            var database = client.GetDatabase(databaseSettings.DatabaseName);
+            Products = database.GetCollection<Product>(databaseSettings.CollectionName);
+            CatalogContextSeed.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
