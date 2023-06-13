dc4064473f56f4ebc334d7ac203f6e825b91a6e5
Ahmed Bayoumy <Ahmed.Bayoumy@linkdev.com>
2023-06-13 12:10:43 +0300

    add discount entities, repositories and controller

---
+++ src/Services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,53 @@
+﻿using Discount.API.Entities;
+using Discount.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Discount.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class DiscountController : ControllerBase
+    {
+        private readonly IDiscountRepository _discountRepositroy;
+
+        public DiscountController(IDiscountRepository discountRepositroy)
+        {
+            _discountRepositroy = discountRepositroy;
+        }
+
+        [HttpGet("{productName}",Name = "GetDiscount")]
+        [ProducesResponseType(typeof(Coupon),(int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> GetDiscount(string productName)
+        {
+            var coupon = await _discountRepositroy.GetCoupon(productName);
+            return Ok(coupon);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Coupon),(int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> CreateDiscount([FromBody] Coupon coupon)
+        {
+            await _discountRepositroy.CreateCoupon(coupon);
+            return CreatedAtRoute("GetDiscount",new { productName = coupon.ProductName },coupon);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> UpdateDiscount([FromBody] Coupon coupon)
+        {
+            return Ok(await _discountRepositroy.UpdateCoupon(coupon));
+        }
+
+        [HttpDelete("{productName}",Name = "DeleteDiscount")]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> DeleteDiscount(string productName)
+        {
+            return Ok(await _discountRepositroy.DeleteCoupon(productName));
+        }
+    }
+}
---
+++ src/Services/Discount/Discount.API/Repositories/DiscountRepository.cs
@@ -0,0 +1,53 @@
+﻿using Dapper;
+using Discount.API.Entities;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.API.Repositories
+{
+    public class DiscountRepository : IDiscountRepository
+    {
+        private readonly NpgsqlConnection connection;
+        public DiscountRepository(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetValue<string>("DatabaseSettings:ConnectionString");
+            connection = new NpgsqlConnection(connectionString);
+        }
+
+        public async Task<Coupon> GetCoupon(string productName)
+        {
+            var getQuery = "Select * From Coupon Where ProductName = @ProductName";
+            var coupon = await connection.QueryFirstOrDefaultAsync<Coupon>(getQuery,new { productName = productName });
+            if (coupon == null)
+                return new Coupon() { ProductName = "No Discount", Amount = 0, Description = "No Discount Desc" };
+
+            return coupon;
+        }
+
+        public async Task<bool> CreateCoupon(Coupon coupon)
+        {
+            var createQuery = "Insert Into Coupon(ProductName,Amount,Description) Values(@ProductName,@Amount,@Description)";
+            var affected = await connection.ExecuteAsync(createQuery,new { ProductName = coupon.ProductName, Amount = coupon.Amount, Description = coupon.Description });
+            return affected > 0;
+        }
+        public async Task<bool> UpdateCoupon(Coupon coupon)
+        {
+            var updateQuery = "Update Coupon set ProductName = @ProductName,Amount = @Amount,Description = @Description Where Id = @Id";
+            var affected = await connection.ExecuteAsync(updateQuery, new { ProductName = coupon.ProductName, Amount = coupon.Amount, Description = coupon.Description, Id = coupon.Id });
+            return affected > 0;
+        }
+
+        public async Task<bool> DeleteCoupon(string productName)
+        {
+            var deleteQuery = "Delete From Coupon where ProductName = @ProductName";
+            var affected = await connection.ExecuteAsync(deleteQuery, new { ProductName = productName });
+            return affected > 0;
+        }
+
+        
+    }
+}
