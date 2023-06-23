7290fd3167a3d00b4d046c39ea46a40836dd2194
Ahmed Bayoumy <Ahmed.Bayoumy@linkdev.com>
2023-06-23 20:33:05 +0300

    Add Discount.Grpc project

--- src/Services/Discount/Discount.Grpc/Repositories/DiscountRepository.cs
+++ src/Services/Discount/Discount.Grpc/Repositories/DiscountRepository.cs
@@ -1,5 +1,5 @@
 ﻿using Dapper;
-using Discount.API.Entities;
+using Discount.Grpc.Entities;
 using Microsoft.Extensions.Configuration;
 using Npgsql;
 using System;
@@ -7,47 +7,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Discount.API.Repositories
+namespace Discount.Grpc.Repositories
 {
     public class DiscountRepository : IDiscountRepository
     {
-        private readonly NpgsqlConnection connection;
+        private readonly NpgsqlConnection _connection;
+
         public DiscountRepository(IConfiguration configuration)
         {
-            var connectionString = configuration.GetValue<string>("DatabaseSettings:ConnectionString");
-            connection = new NpgsqlConnection(connectionString);
+            _connection = new NpgsqlConnection(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
         }
 
         public async Task<Coupon> GetCoupon(string productName)
         {
-            var getQuery = "Select * From Coupon Where ProductName = @ProductName";
-            var coupon = await connection.QueryFirstOrDefaultAsync<Coupon>(getQuery,new { ProductName = productName });
+            var getQuery = "Select * From Coupon where ProductName=@ProductName";
+            var coupon = await _connection.QueryFirstOrDefaultAsync<Coupon>(getQuery);
             if (coupon == null)
                 return new Coupon() { ProductName = "No Discount", Amount = 0, Description = "No Discount Desc" };
-
             return coupon;
         }
 
         public async Task<bool> CreateCoupon(Coupon coupon)
         {
-            var createQuery = "Insert Into Coupon(ProductName,Amount,Description) Values(@ProductName,@Amount,@Description)";
-            var affected = await connection.ExecuteAsync(createQuery,new { ProductName = coupon.ProductName, Amount = coupon.Amount, Description = coupon.Description });
+            var CreateQuery = "Insert Into Coupon(ProductName,Amount,Description) Values(@ProductName,@Amount,@Description)";
+            var affected = await _connection.ExecuteAsync(CreateQuery, new { ProductName = coupon.ProductName, Amount = coupon.Amount, Description = coupon.Description });
             return affected > 0;
         }
+
         public async Task<bool> UpdateCoupon(Coupon coupon)
         {
             var updateQuery = "Update Coupon set ProductName = @ProductName,Amount = @Amount,Description = @Description Where Id = @Id";
-            var affected = await connection.ExecuteAsync(updateQuery, new { ProductName = coupon.ProductName, Amount = coupon.Amount, Description = coupon.Description, Id = coupon.Id });
+            var affected = await _connection.ExecuteAsync(updateQuery, new { ProductName = coupon.ProductName, Amount = coupon.Amount, Description = coupon.Description, Id = coupon.Id });
             return affected > 0;
         }
 
         public async Task<bool> DeleteCoupon(string productName)
         {
             var deleteQuery = "Delete From Coupon where ProductName = @ProductName";
-            var affected = await connection.ExecuteAsync(deleteQuery, new { ProductName = productName });
+            var affected = await _connection.ExecuteAsync(deleteQuery, new { ProductName = productName });
             return affected > 0;
         }
-
-        
     }
 }
